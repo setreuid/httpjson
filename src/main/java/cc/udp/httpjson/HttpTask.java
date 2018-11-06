61504b8c18d57515220d5446deae1a2168a3694f
Setreuid <setreuid@naver.com>
2018-11-06 16:28:40 +0900

    Rollback

--- src/main/java/cc/udp/httpjson/HttpTask.java
+++ src/main/java/cc/udp/httpjson/HttpTask.java
@@ -9,20 +9,12 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.management.RuntimeErrorException;
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
 /**
  * Created by DMDEV on 2017-11-23.
@@ -67,64 +59,27 @@ public class HttpTask extends Thread
         {
             URL url = new URL(this.targetUrl);
             URLConnection conn = (URLConnection) url.openConnection();
-            BufferedReader buff = null;
+            HttpURLConnection hconn = (HttpURLConnection) conn;
 
-            if (this.targetUrl.substring(0, 5).toUpperCase().equals("HTTPS"))
+            hconn.setRequestMethod(this.requestMode);
+            hconn.setDoOutput(true);
+            hconn.setDoInput(true);
+            hconn.setUseCaches(true);
+            hconn.setDefaultUseCaches(true);
+
+            if (this.params.size() > 0 && this.requestMode.equals("POST"))
             {
-                trustAllHosts();
+                byte[] postParams = this.getEncodedParams().getBytes("UTF-8");
+                hconn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                hconn.setRequestProperty("Content-Length", String.valueOf(postParams.length));
 
-                HttpsURLConnection hconn = (HttpsURLConnection) conn;
-
-                hconn.setHostnameVerifier(DO_NOT_VERIFY);
-                hconn.setRequestMethod(this.requestMode);
-                hconn.setDoOutput(true);
-                hconn.setDoInput(true);
-                hconn.setUseCaches(true);
-                hconn.setDefaultUseCaches(true);
-
-                if (this.params.size() > 0 && this.requestMode.equals("POST"))
-                {
-                    byte[] postParams = this.getEncodedParams().getBytes("UTF-8");
-                    hconn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-                    hconn.setRequestProperty("Content-Length", String.valueOf(postParams.length));
-
-                    OutputStream writer = hconn.getOutputStream();
-                    writer.write(postParams);
-                    writer.flush();
-                    writer.close();
-                }
-
-                buff = new BufferedReader(new InputStreamReader (hconn.getInputStream(), "UTF-8"));
-            }
-            else
-            {
-                HttpURLConnection hconn = (HttpURLConnection) conn;
-
-                hconn.setRequestMethod(this.requestMode);
-                hconn.setDoOutput(true);
-                hconn.setDoInput(true);
-                hconn.setUseCaches(true);
-                hconn.setDefaultUseCaches(true);
-
-                if (this.params.size() > 0 && this.requestMode.equals("POST"))
-                {
-                    byte[] postParams = this.getEncodedParams().getBytes("UTF-8");
-                    hconn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-                    hconn.setRequestProperty("Content-Length", String.valueOf(postParams.length));
-
-                    OutputStream writer = hconn.getOutputStream();
-                    writer.write(postParams);
-                    writer.flush();
-                    writer.close();
-                }
-
-                buff = new BufferedReader(new InputStreamReader (hconn.getInputStream(), "UTF-8"));
+                OutputStream writer = hconn.getOutputStream();
+                writer.write(postParams);
+                writer.flush();
+                writer.close();
             }
 
-            if (buff == null)
-            {
-                throw new RuntimeException();
-            }
+            BufferedReader buff = new BufferedReader(new InputStreamReader (hconn.getInputStream(), "UTF-8"));
 
             while((tempContents = buff.readLine()) != null)
             {
@@ -138,41 +93,6 @@ public class HttpTask extends Thread
         {
             e.printStackTrace();
             throw new RuntimeException();
-        }
-    }
-
-    final HostnameVerifier DO_NOT_VERIFY = new HostnameVerifier() {
-        public boolean verify(String hostname, SSLSession session) {
-            return true;
-        }
-    };
-
-    private void trustAllHosts() {
-        // Create a trust manager that does not validate certificate chains
-        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                return new java.security.cert.X509Certificate[] {};
-            }
-
-            @Override
-            public void checkClientTrusted(X509Certificate[] chain,
-                                           String authType) throws CertificateException {
-            }
-
-            @Override
-            public void checkServerTrusted(X509Certificate[] chain,
-                                           String authType) throws CertificateException {
-            }
-        }};
-
-        // Install the all-trusting trust manager
-        try {
-            SSLContext sc = SSLContext.getInstance("TLS");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-        }
-        catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
